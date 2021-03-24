ccd618305d0a2e9548f61471afcc43f8d612fad5
William Paredes <willyparedes5@gmail.com>
2021-03-23 21:01:47 -0700

    adding matrix

--- main.py
+++ main.py
@@ -5,15 +5,21 @@ import sys
 from datetime import datetime
 from currenttime import current_time
 
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
+
 from PIL import Image, ImageDraw, ImageFont
 screen_size = 64,32
 inital_text_location = 4, 10
 
 image = Image.new("RGB", screen_size)
-
 image.thumbnail(screen_size, Image.ANTIALIAS)
 
-
+#Configurations for Matrix 
+options = RGBMatrixOptions()
+options.rows = 32
+options.cols = 64
+options.parallel = 1
+options.hardware_mapping = 'adafruit-hat'
 
 
 
@@ -25,3 +31,13 @@ def generate_text(im, text, begin_loc, font_size):
 
 generate_text(image, current_time(), inital_text_location, 16)
 image.show()
+matrix.SetImage(image.convert('RGB'))
+
+
+try:
+    print("Press CTRL-C to stop")
+    while True:
+        time.sleep(100)
+except KeyboardInterrupt:
+    sys.exit(0)
+    
